e49b2da2acc129a34e8247509da7c86f4aeeef5c
github-action-benchmark <github@users.noreply.github.com>
2025-03-12 16:06:44 +0000

    add Rust Benchmark (cargo) benchmark result for 4de79c901d37955a97f07a2b8dd5cde51c42379f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741795423177,
+  "lastUpdate": 1741795604323,
   "repoUrl": "https://github.com/simoncozens/fontgrep",
   "entries": {
     "Rust Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "name": "bench_name_ofl",
             "value": 5948386.25,
             "range": "± 243908.65",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "simon@simon-cozens.org",
+            "name": "Simon Cozens",
+            "username": "simoncozens"
+          },
+          "committer": {
+            "email": "simon@simon-cozens.org",
+            "name": "Simon Cozens",
+            "username": "simoncozens"
+          },
+          "distinct": true,
+          "id": "4de79c901d37955a97f07a2b8dd5cde51c42379f",
+          "message": "This bit not needede",
+          "timestamp": "2025-03-12T16:03:35Z",
+          "tree_id": "42f7e3d1f6d6eaf874d7afa39d5d684640cc3e14",
+          "url": "https://github.com/simoncozens/fontgrep/commit/4de79c901d37955a97f07a2b8dd5cde51c42379f"
+        },
+        "date": 1741795603709,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_name_ofl",
+            "value": 5873104.25,
+            "range": "± 299442.90",
             "unit": "ns/iter"
           }
         ]
