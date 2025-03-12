719779b81c88beb98a61912ad725783522e9f476
github-action-benchmark <github@users.noreply.github.com>
2025-03-12 16:03:43 +0000

    add Rust Benchmark (cargo) benchmark result for 9ac079079f4dcedbe818898bbf912f0c23d25286

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1741795423177,
+  "repoUrl": "https://github.com/simoncozens/fontgrep",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "simon@simon-cozens.org",
+            "name": "Simon Cozens",
+            "username": "simoncozens"
+          },
+          "committer": {
+            "email": "simon@simon-cozens.org",
+            "name": "Simon Cozens",
+            "username": "simoncozens"
+          },
+          "distinct": true,
+          "id": "9ac079079f4dcedbe818898bbf912f0c23d25286",
+          "message": "Argh",
+          "timestamp": "2025-03-12T15:57:42Z",
+          "tree_id": "6baf9fc28d31dacaf3ddee398a52b57bd3795dcf",
+          "url": "https://github.com/simoncozens/fontgrep/commit/9ac079079f4dcedbe818898bbf912f0c23d25286"
+        },
+        "date": 1741795422155,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_name_ofl",
+            "value": 5948386.25,
+            "range": "± 243908.65",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
